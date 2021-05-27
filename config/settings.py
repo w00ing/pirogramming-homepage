42948a15e51315742fedd14458d7180ed42f4eb4
Woo-yeong Kim <kimwy1997@gmail.com>
2021-05-28 02:26:51 +0900

    Add recruiting info for 15

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-print(DEBUG)
+print(f"DEBUG: {DEBUG}")
 
 
 ALLOWED_HOSTS = ["*"]
